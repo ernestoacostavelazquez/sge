a066c91ee4018852b15aca45e2b4d2b00f524dfc
ernestoacostavelazquez <ernesto.acostavelazquez@gmail.com>
2024-11-26 11:12:37 -0600

    actualización 26/11/2024

--- src/app/features/vehiculos/components/marcas/marcas.component.ts
+++ src/app/features/vehiculos/components/marcas/marcas.component.ts
@@ -1,12 +1,19 @@
+// marcas.compenents.ts
+import { CommonModule, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertService } from '../../../../shared/services/alert.service';
 
 @Component({
   selector: 'app-marcas',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,HttpClientModule,JsonPipe,NgFor,NgIf, CommonModule],
   templateUrl: './marcas.component.html',
   styleUrl: './marcas.component.css'
 })
 export class MarcasComponent {
+ 
+  
 
 }
